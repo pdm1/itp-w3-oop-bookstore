fdf2bbf938158ce8d9a5a13cc7f311965e720e63
Patrick Matthies <coding.newb80@gmail.com>
2017-01-27 00:10:01 +0000

    Solution

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,40 @@
 class Bookstore(object):
-    pass
+    def __init__(self,name):
+        self.name = name
+        self.books = []
 
+    def get_books(self):
+        return self.books
+        
+    def add_book(self,book=None):
+        self.books.append(book)
+    
+    def search_books(self,author=None, title=None):
+        books = []
 
+        for book in self.books:
+            if title is None:
+                if author == book.author:
+                    books.append(book)
+            elif author is None:
+                if title.lower() in book.title.lower():
+                    books.append(book)
+            else:
+                if author == book.author and title.lower() in book.title.lower():
+                    books.append(book)
+        return books
+        
 class Author(object):
-    pass
-
-
+    def __init__(self,name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []                 
+        
+    def get_books(self):
+        return self.books               
+        
 class Book(object):
-    pass
+    def __init__(self, title, author): 
+        self.title = title             
+        self.author = author              
+        author.books.append(self)  
